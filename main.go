ef4a81e1e74ab8c6677152cfb070ab61202b6319
ranen <189527031@qq.com>
2023-03-21 10:12:35 +0800

    feat: add health

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"code": 0,
+			"msg":  "success!",
+		})
+	})
+	r.Run()
 }
